87952b4fc7d589d8e9031b99c68460c3c2fc8944
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-01-03 14:13:42 +0000

    train_fft: seems to do OK, going to add smoothing and try out full-signal convolution

--- accumulators/fft.py
+++ accumulators/fft.py
@@ -35,7 +35,7 @@ def fft(arr, nn, isign):
 			i = m
 			while i < nn:
 				j = i+imax 
-				print m, j, i, imax, istep, nn
+				# print m, j, i, imax, istep, nn
 				tmp_re = wr * rp[j][0] - wi*rp[j][1]
 				tmp_im = wr * rp[j][1] + wi*rp[j][0]
 				tmp_j = (rp[i][0]-tmp_re, rp[i][1]-tmp_im)
@@ -58,12 +58,12 @@ def fft(arr, nn, isign):
 			im *= scale 
 			rp[i] = (re, im)
 
-	return rp
+	return rp.values()
 
 def ccmult(c1, c2):
 
 	first_re, first_im = c1
-	secnd_re, secnd_re = c2
+	secnd_re, secnd_im = c2
 
 	ac = first_re * secnd_re
 	bd = first_im * -secnd_im
@@ -77,6 +77,7 @@ def pad_sim(arr, amount):
 	ret = [(0, 0) for i in range(amount)]
 	ret += arr 
 	ret += [(0, 0) for i in range(amount)]
+	return ret
 
 def next_pow2(of):
 	cur = 1
@@ -88,18 +89,31 @@ def next_pow2(of):
 def pad_end(arr, amount):
 	return arr + [(0, 0) for i in range(amount)]
 
+def correlate_skip_fft(sig1, sig2):
+	cnv = [ccmult(i, j) for i,j in zip(sig1_fft, sig2_fft)]
+	res = fft(cnv, power_pad, -1)
+
 def correlate(sig1, sig2):
 
 	sig1 = [cnv_complex(i) for i in sig1]
 	sig2 = [cnv_complex(i) for i in sig2]
 
-	padding = abs(len(sig1) - len(sig2))/2
+	if len(sig1) % 2 != 0:
+		sig1 = pad_end(sig1, 1)
+	if len(sig2) % 2 != 0:
+		sig2 = pad_end(sig2, 1)
+
+	#print len(sig1), len(sig2), abs(len(sig1) - len(sig2)),
+	padding = (abs(len(sig1) - len(sig2)))/2
+	#print padding
 	if len(sig1) < len(sig2):
 		sig1 = pad_sim(sig1, padding)
 	elif len(sig2) < len(sig1):
 		sig2 = pad_sim(sig2, padding)
 
-	assert(len(sig1) == len(sig2))
+	if not len(sig1) == len(sig2):
+		print len(sig1), len(sig2)
+		raise ValueError()
 
 	power_pad = next_pow2(len(sig1))
 	pad_toadd = power_pad - len(sig1)
@@ -109,6 +123,8 @@ def correlate(sig1, sig2):
 
 	sig1_fft = fft(sig1, power_pad, 1)
 	sig2_fft = fft(sig2, power_pad, 1)
+
+#	print sig1_fft, sig2_fft
 
 	cnv = [ccmult(i, j) for i,j in zip(sig1_fft, sig2_fft)]
 	res = fft(cnv, power_pad, -1)
